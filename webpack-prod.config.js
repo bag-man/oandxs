74fe8d4f9ceec55b5ae6c69ae3fd5f237943deed
Owen Garland <owen.garland@clock.co.uk>
2016-10-26 12:42:17 +0100

    Added webpack production and postinstall for Heroku

---
+++ webpack-prod.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map'
+, entry: [
+    './assets/js/index'
+  ]
+, output: {
+    path: path.join(__dirname, 'assets', 'build', 'js')
+  , filename: 'bundle.js'
+  , publicPath: '/assets/build/js/'
+  }
+, plugins: [
+    new webpack.optimize.DedupePlugin()
+  , new webpack.optimize.UglifyJsPlugin({
+      minimize: true
+    , compress: {
+        warnings: false
+      }
+    })
+  ]
+, module: {
+    loaders: [
+      { test: /\.js$/
+      , loader: 'babel-loader'
+      , query: {
+          presets: [ 'es2015' ]
+        }
+      , include: path.join(__dirname, 'assets/js')
+      }
+    ]
+  }
+}
